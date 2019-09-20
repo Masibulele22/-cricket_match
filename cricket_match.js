5ab32b7741686522e39c8824a6d2c0ee4c07d38e
Masibulele22 <sticky.ngondo@gmail.com>
2019-09-20 16:45:22 +0200

    first deploy

--- cricket_match.js
+++ cricket_match.js
@@ -2,43 +2,71 @@ module.exports = CricketScoreKeeper;
 
 function CricketScoreKeeper() {
     var score = 0;
+    var wickets = 10;
+    let over = 8;
 
-    function scoreCounter() {
-        if (run == "-") {
-            return score+= 0;
+    function scoreCounter(overString) {
+
+        var overBalls = overString.split('');
+
+        for (var i = 0; i < overBalls.length; i++) {
+
+            var eachBall = overBalls[i];
+
+            if (eachBall == 1) {
+                score += 1;
+            }
+            else if (eachBall == 2) {
+                score += 2;
+            }
+            else if (eachBall == 3) {
+                score += 3;
+            }
+            else if (eachBall == 4) {
+                score += 4;
+            }
+            else if (eachBall == 6) {
+                score += 6;
+            }
+            else {
+                score += 0;
+            }
+
+        };
+        return score;
+    };
+
+    function oversLeft(overString) {
+        var overStringSplit2 = overString.split('');
+        for (i = 0; i < overStringSplit2.length; i++) {
+            var stringLengthLine = overStringSplit2[i];        
         }
-        else if (run == "1") {
-            return score+= 1;
-        }
-        else if (run == "2") {
-            return score+= 2;
-        }
-        else if (run == "3") {
-            return score+= 3;
-        }
-        else if (run == "4") {
-            return score+= 4;
-        }
-        else if (run == "5") {
-            return score+= 5;
-        }
-        else if (run == "6") {
-            return score+= 6;
-        }
-        else if (run == "W" || "w") {
-            return score+= 0;
-        }
-        else {
-            return score+= 0;
+        if (stringLengthLine.length == 6) {
+            return over--
         }
     };
 
-    function getScore(){
+    function wicketsFallen(overString) {
+
+        var overStringSplit = overString.split('');
+        for (var i = 0; i < overStringSplit.length; i++) {
+
+            var wicketDectector = overStringSplit[i];
+            if (wicketDectector = "w") {
+                return wickets--
+            };
+        }
+        return wicketDectector;
+    };
+
+    function getScore() {
         return score
     };
 
     return {
         scoreCounter,
+        oversLeft,
+        wicketsFallen,
         getScore
     };
 };
--- test/cricket_match.test.js
+++ test/cricket_match.test.js
@@ -3,17 +3,52 @@ let CricketScoreKeeper = require('../cricket_match');
 
 describe('CricketScoreKeeper', function(){
 
-    it('should show and count the current score', function(){
+    it('should show the number of overs left.', function(){
+        var cricketScoreKeeper = CricketScoreKeeper(10);
 
-        var numberOfRuns = CricketScoreKeeper();
-        numberOfRuns.getScore("1");
-        numberOfRuns.getScore("1");
-        numberOfRuns.getScore("1");
-        numberOfRuns.getScore("1");
-        numberOfRuns.getScore("1");
-        numberOfRuns.getScore("1");
+        cricketScoreKeeper.oversLeft("----1-");
 
-        assert.equal(6, numberOfRuns(6));
+        assert.equal(9, cricketScoreKeeper.oversLeft());
+    });
+
+    it('should show the current score.', function(){
+        var cricketScoreKeeper3 = CricketScoreKeeper();
+        assert.equal(cricketScoreKeeper3.scoreCounter('-----1'), 1);
+    });
+
+    it('should show the score.', function(){
+        var cricketScoreKeeper2 = CricketScoreKeeper();
+
+        cricketScoreKeeper2.getScore("11--11");
+
+        assert.equal(cricketScoreKeeper2.getScore('4'), 4);
+    });
+
+    it('should show the number of decremented overs', function(){
+
+        var doneOvers = CricketScoreKeeper();
+
+        doneOvers.oversLeft("-----w");
+
+        assert.equal(doneOvers.oversLeft("-----w"), 7);
+    });
+
+    it('should show the number of wickets taken', function(){
+
+        var fallenWickets = CricketScoreKeeper();
+
+        fallenWickets.wicketsFallen("-----w");
+
+        assert.equal(fallenWickets.wicketsFallen("-----w"), 9);
+    });
+    
+    it('should show the number of wickets taken 2', function(){
+
+        var fallenWickets2 = CricketScoreKeeper();
+
+        fallenWickets2.wicketsFallen("--w--w");
+
+        assert.equal(fallenWickets2.wicketsFallen("--w--w"), 9);
     });
 
 });
